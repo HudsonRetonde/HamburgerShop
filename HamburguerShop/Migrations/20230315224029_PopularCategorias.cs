9c7163f3dbeb529558aaf628d6a5eb1faa04b8d9
Hudson Retonde <hudsonsretonde@gmail.com>
2023-03-15 20:31:05 -0300

    Migrations Categoria e Lanches e população de ambas

---
+++ HamburguerShop/Migrations/20230315224029_PopularCategorias.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HamburguerShop.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopularCategorias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Categorias(CategoriaNome, Descricao)" +
+                "VALUES('Normal', 'Lanches feitos com igredientes normais')");
+
+			migrationBuilder.Sql("INSERT INTO Categorias(CategoriaNome, Descricao)" +
+				"VALUES('Naturais', 'Lanches feitos com igredientes naturais e integrais')");
+
+		}
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Categorias");
+        }
+    }
+}
